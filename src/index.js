02e0b06605f80b23ae50c21431c870d188bbac1f
Cody Condon <codycondon50@gmail.com>
2016-02-28 19:52:14 -0500

    applied state of search bar and reassign state to value on each character change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyAO5w8EnsCAM5PPAgHThSu6SeEA3qMxYl0";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component. This component should produce some html
+
+const App = () => {
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
+}
+//Take this components generated html and put it on the page
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
