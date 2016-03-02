ecc2a3f89069153c05fea2fd21d62ba9d689a04b
Cody Condon <codycondon50@gmail.com>
2016-03-02 09:36:42 -0500

    clean up code for review

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,19 +3,24 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
-    this.state = { term: ''};
+    this.state = { term: ''}; // Initial state is blank
   }
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
      <input
       value ={this.state.term}
-      onChange={event => this.setState({term: event.target.value})}/>
-
+      onChange={event => this.onInputChange(event.target.value)}/>
+      {/*  above is saying that on key change run new function syntax which takes event and sets new state to the event target value
+        then stores it in value by using this.state.term */}
      </div>
    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
